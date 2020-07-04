c8fbb99cd58ce28571c1b07887d8d7d04b42ac7b
jonathanlurie <lurie.jo@gmail.com>
2020-07-04 15:22:59 +0200

    WIP day stats

--- src/core/Day.js
+++ src/core/Day.js
@@ -21,6 +21,48 @@ class Day {
     this._alldevEvents.push(de)
     this._devEventsPerHour[de.hour].push(de)
   }
+
+
+  getEvents(options = {}) {
+    const hour = 'hour' in options ? options.hour : null
+    const type = 'type' in options ? options.type : null
+
+    let events = []
+    if (hour && hour <=23 && hour >= 0) {
+      events = this._devEventsPerHour[hour].slice()
+    } else {
+      events = this._alldevEvents.slice()
+    }
+
+    if (type) {
+      events = events.filter((de) => de.type.toLowercase() === type.toLowercase())
+    }
+
+    return events
+  }
+
+
+  getNumberOfEvents(options = {}) {
+    return this.getEvents(options).length
+  }
+
+
+  getPeakHour(type) {
+    let maxEvents = 0
+    let hour = 0
+
+    for (let i = 0; i < 24; i += 1) {
+      if (this._devEventsPerHour[i].length > maxEvents) {
+        maxEvents = this._devEventsPerHour[i].length
+        hour = i
+      }
+    }
+
+    return {
+      maxEvents,
+      hour
+    }
+  }
   
 }
 
